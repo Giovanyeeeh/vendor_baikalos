9f5c805a71ce973335b2ef020934fdc6e57c65f9
Sergey Vasiliev <sdv22@live.ru>
2022-02-16 15:24:24 +0300

    Support security patch date override for safetynet
    Change-Id: I0a033e2163df62b74066ff4322d3d6b9fe4c10c9

--- build/core/main.mk
+++ build/core/main.mk
@@ -5,6 +5,7 @@ include $(TOPDIR)vendor/baikalos/build/core/main_version.mk
 ifeq ($(PRODUCT_OVERRIDE_INFO),true)
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.build.stock_sec_patch=$(PRODUCT_OVERRIDE_SECURITY_PATCH)
 
 # Override product info for Google Play Services and SafetyNet
 ADDITIONAL_BUILD_PROPERTIES += \
